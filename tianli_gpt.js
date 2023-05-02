e2f6530d1ae2795a5905c433dd76f386ed4edb28
Zkeq <admin@icodeq.com>
2023-05-02 16:21:28 +0800

    CatGPT Fix nocontent err (2).

--- tianli_gpt.js
+++ tianli_gpt.js
@@ -146,7 +146,7 @@ var tianliGPT = {
 function runTianliGPT() {
   insertAIDiv(tianliGPT_postSelector);
   const content = tianliGPT.getTitleAndContent();
-  if (!content && content !== '') {
+  if (content && content !== '') {
     console.log('TianliGPT本次提交的内容为：' + content);
   }else{
     return;
